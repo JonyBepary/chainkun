30e82aa2f2d1cfdc6e22283ae8ef31491cd321ad
Sohel Ahmed Jony <sohelahmedjony@gmail.com>
2024-08-12 08:00:51 +0600

    Enhance initialization and configuration handling in main function
    - Added `CheckChainkunFolder` function to verify the existence of the `.chainkun` folder and `config.yaml`
    - Improved the main function to handle environment variables and default configuration paths
    - Ensured proper initialization of the `.chainkun` folder and configuration file if they do not exist
    - Included error handling for reading configuration files
    - Refactored code for better readability and maintainability

--- main.go
+++ main.go
@@ -4,36 +4,56 @@ import (
 	"fmt"
 	"os"
 	"path"
-	"sync"
 
 	"github.com/JonyBepary/chainkun/internal"
+	"github.com/davecgh/go-spew/spew"
 )
+
+// check .chainkun folder exists and .chainkun/config.yaml exists
+// if not create .chainkun folder and create config.yaml
+func CheckChainkunFolder(roo string) bool {
+	// check if chainkun folder exists
+	_, err := os.Stat(path.Join(roo, "config.yaml"))
+	if err == nil {
+		fmt.Println("CHAINKUN FOLDER and Config file EXISTS")
+		return true
+	}
+
+	// check config.yaml exists
+	return false
+
+}
 
 func main() {
 	// CHECK ENV VARIABLE OR DEFAULT CONFIG PATH AVAILABLE
 	// Check if the CONFIG_FILE environment variable is set
-	var wg sync.WaitGroup
+	// var wg sync.WaitGroup
 	// wg.Add(1)
 	// go func() {
 	// 	defer wg.Done()
-
-	if configPath, ok := os.LookupEnv("CHAINKUN_ENV"); ok {
-		internal.InitChaincode(configPath)
+	if ok := CheckChainkunFolder(path.Join(os.Getenv("HOME"), ".chainkun")); !ok {
+		fmt.Println("CHAINKUN FOLDER DOES NOT EXISTS")
+		if configPath, ok := os.LookupEnv("CHAINKUN_ENV"); ok {
+			internal.InitChaincode(configPath)
+		} else {
+			internal.InitChaincode(path.Join(os.Getenv("HOME"), ".chainkun"))
+		}
+	}
+	var cfg internal.ConfigStruct
+	var err error
+	// if configFile pass then read the config file else read the default config file
+	if len(os.Args) > 1 {
+		cfg, err = internal.ReadCfgFile(os.Args[1])
+		if err != nil {
+			fmt.Println("Error reading config file:", err)
+		}
 	} else {
-		internal.InitChaincode(path.Join(os.Getenv("HOME"), ".chainkun"))
+		cfg, err = internal.ReadCfgFile(path.Join(os.Getenv("HOME"), ".chainkun", "config.yaml"))
+		if err != nil {
+			fmt.Println("Error reading config file:", err)
+		}
 	}
-	// }()
-
-	wg.Wait()
-
-	argsWithoutProg := os.Args[1:]
-	cfgPath := internal.GetCfgPath(argsWithoutProg)
-
-	cfg, err := internal.InitConfig(cfgPath)
-	if err != nil {
-		fmt.Println("ERROR:", err)
-		return
-	}
-	fmt.Println(cfg)
+	spew.Dump(cfg)
+	internal.FabricDownload(&cfg)
 
 }
